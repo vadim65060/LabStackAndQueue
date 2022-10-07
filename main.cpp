793446d59e66ddc7af580499dd8d6c25908eaba2
vadim65060 <vadim65060@users.noreply.github.com>
2022-10-07 14:47:56 +0300

    add queue test

--- main.cpp
+++ main.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 #include "Stack.h"
+#include "Queue.h"
 
-void test(){
-    Stack<int>st(1);
+void TestStack() {
+    Stack<int> st(1);
     st.Push(2);
     st.Push(3);
     st.Push(4);
     st.Push(5);
-    Stack<int>a(st);
+    Stack<int> a(st);
     st.SetFront(-1);
     st.PrintNods();
     a.PrintNods();
 }
 
+void TestQueue() {
+    Queue<int> queue(1);
+    queue.PushBack(2);
+    queue.PrintNods();
+    queue.PopFront();
+    queue.PrintNods();
+    Queue<int> q2(queue);
+    q2.PopFront();
+    std::cout << "q2 empty: " << q2.IsEmpty();
+}
+
 int main() {
-    test();
+    TestQueue();
+
 }
